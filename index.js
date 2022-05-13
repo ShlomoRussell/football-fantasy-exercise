bfa6b2e3e77da940fc2a0871f7b8c71db2c3fd2e
bearddude1 <baapteshuva@gmail.com>
2022-05-13 10:17:58 +0300

    fixed conflicts

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
 const express = require('express');
 const app = express();
-const path = require('path')
-const uuid = require('uuid').v4
-const typedefs = require('./typedefs')
-const PORT = 5678;
+const PORT = 8989;
+const dal = require('./DAL');
 
-app.use("/", express.static('public'));
+// Routers
+const playersCtrl = require('./controllers/player.ctrl');
+const teamCtrl = require('./controllers/team.ctrl');
+
+app.use('/', express.static('public'));
+
 app.use(express.urlencoded({
-    extended: true
-}))
+    extended: true,
+}));
+app.use(express.json());
 
-app.get('/api/team', function(req, res, next) {
-    res.sendFile(path.join(__dirname, 'data/teams.json'));
-})
+app.use('/api/player', playersCtrl);
+app.use('/api/team', teamCtrl);
 
-app.get('/api/player', function(req, res, next) {
-    res.sendFile(path.join(__dirname, 'data/players.json'));
-})
-app.get('/api/player/position', function(req, res) {
-    res.send(["Forward",
-        "Midfielder",
-        "Defender",
-        "Goalkeeper"
-    ]);
-})
 
-app.post('/api/player', function(req, res) {
-    console.log(req.body)
-        /**@type{typedefs.FootballPlayer} */
-    const player = {
-        id: uuid(),
-        name: req.body['player-name'],
-        position: req.body['player-position'],
-        teamid: req.body['player-team'],
-    }
-    res.send(player)
-})
-app.listen(PORT, () => console.log(`server started at port ${ PORT }`))
+app.listen(PORT, () => console.log(`server started at port ${PORT}`))
